1aaccbdc9e5a50ac740c6fc1d5e23d9d15346e6b
schlad3k <simonchladek4@gmail.com>
2026-04-08 10:46:42 +0200

    feat: add spring boot main class and couchbase config

---
+++ app/src/main/java/at/htlwrn/couchbase/CouchbaseApp.java
@@ -0,0 +1,11 @@
+package at.htlwrn.couchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchbaseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CouchbaseApp.class, args);
+    }
+}
